e4b510728cecbab0cf50afa6d187cf2e98ee902e
AliAbdulle <alidhobey@gmail.com>
2020-05-21 19:30:46 -0500

    link working properly

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Link, Switch,  Route} from 'react-router-dom'
 
 import Home from './Home'
 import About from './About'
+import Contact from './Contact'
 
 function App() {
   
@@ -11,10 +12,12 @@ function App() {
     <div>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
 
         <Switch>
             <Route exact path="/"><Home /></Route>
             <Route path="/about" > <About /> </Route>
+            <Route path="/contact" > <Contact /> </Route>
         </Switch>
     </div>
   )
