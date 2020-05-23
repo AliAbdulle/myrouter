686c61b4cf12b7e543ff21c306f7f8fef00b6940
AliAbdulle <alidhobey@gmail.com>
2020-05-22 21:59:55 -0500

    Finished final one

--- src/Components/Portfolio/App.js
+++ src/Components/Portfolio/App.js
@@ -1,1 +1,29 @@
 
+import React from "react"
+//import Header from "./components/Header"
+//import Footer from "./components/Footer"
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profiles/Profile"
+
+import {Switch, Route} from "react-router-dom"
+
+function App() {    
+    return (
+        <div>
+            {/* <Header /> */}
+            
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/profile">
+                    <Profile/>
+                </Route>
+            </Switch>
+
+        {/* <Footer /> */}
+        </div>
+    )
+}
+
+export default App
