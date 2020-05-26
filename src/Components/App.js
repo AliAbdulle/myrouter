357296b3d0262c7986c50c619a467cda0e55e783
AliAbdulle <alidhobey@gmail.com>
2020-05-25 21:20:47 -0500

    life saved the apps

---
+++ src/Components/App.js
@@ -0,0 +1,55 @@
+//import React from 'react';
+//import {Link, Switch,  Route} from 'react-router-dom'
+
+
+// import Home from './Home'
+// import About from './About'
+// import Contact from './Contact'
+
+// function App() {
+  
+//   return (
+//     <div>
+//         <Link to="/">Home</Link>
+//         <Link to="/about">About</Link>
+//         <Link to="/contact">Contact</Link>
+
+//         <Switch>
+//             <Route exact path="/"><Home /></Route>
+//             <Route path="/about" > <About /> </Route>
+//             <Route path="/contact" > <Contact /> </Route>
+//         </Switch>
+//     </div>
+//   )
+// }
+
+//export default App;
+
+import React from "react"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./Portfolio/pages/Home"
+import Profile from "./pages/../Profiles"
+
+import {Switch, Route} from "react-router-dom"
+
+function App() {    
+    return (
+        <div>
+            <Header />
+            
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/profile">
+                    <Profile/>
+                </Route>
+            </Switch>
+            
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
